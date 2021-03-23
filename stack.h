1ca9489a02f8dcbbad962dd51bc962e818feb60e
Millena Freitas <freitasmillena27@gmail.com>
2021-03-23 00:27:36 +0000

    Início da stack implementada. TODO push pop

--- stack.h
+++ stack.h
@@ -1,1 +1,38 @@
-//Arquivo para implementação da Stack
+#include <stdio.h>
+#include <stdlib.h>
+
+//representar stack através de uma estrutura
+
+typedef struct {
+    int sp; //stack pointer 
+    long *elements; 
+    int capacity;
+
+}Stack;
+
+void push(Stack *s, long n);
+int pop(Stack *s);
+
+//Cria uma stack vazia
+
+Stack* create_stack() {
+    Stack* s = (Stack*)malloc(sizeof(Stack)); //alocar espaço para a stack
+    s->sp = -1; //inicializa stack pointer em -1, ou seja, stack está vazia
+    s->capacity = 10240;
+    s->elements = (long*)malloc(s->capacity * sizeof(long));
+    
+    return s;
+} 
+
+//Verifica se a stack está vazia
+int empty_stack(Stack* s) {
+    
+    return (s->sp == -1); //se for verdadeiro 1, se for falso 0 
+}
+
+//Verifica se a stack está cheia
+int full_stack(Stack* s) {
+
+    return (s->sp == s->capacity - 1); //capacity - 1 é o último elemento do array, caso esteja preenchido, logo, a stack está cheia
+
+}
