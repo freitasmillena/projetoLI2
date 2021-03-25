1315395f69a84bb884d15c28b808ef81c2b71dcc
Gonçalo Gomes <goncaloagomes28@gmail.com>
2021-03-25 23:37:52 +0000

    stack documentanda e .gitignore criado

--- stack.h
+++ stack.h
@@ -1,68 +1,101 @@
+/**
+ * @file stack.h 
+ * 
+ * Module : Guião1 (1a semana)
+ * Description : Stack -> (Guião1) - Trabalho de LI2 2020/2021
+ * 
+ * Neste ficheiro stack.h tem-se como objetivo a implementação da stack. Stack é uma estrutura de dados linear que segue uma ordem particular para a qual as operações são realizadas e caracteriza um empilhamento de dados. A ordem a ser utilizada é **Last In First Out**.
+ * 
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
-//representar stack através de uma estrutura
-
+/**
+ * \brief Representação da stack através de uma estrutura
+ */
 typedef struct {
-    int sp; //stack pointer 
-    long *elements; 
-    int capacity;
+    int sp; ///< Stack pointer -> Aponta para o elemento no topo da Stack
+    long *elements; ///< Array para conter os elementos da Stack
+    int capacity; ///< Capacidade total atual da Stack
 
 }Stack;
 
 void push(Stack *s, long n);
 int pop(Stack *s);
 
-//Cria uma stack vazia
-
+/**
+ * \brief Função que cria uma stack vazia
+ * 
+ * @returns s , Stack criada
+ */
 Stack* create_stack() {
-    Stack* s = (Stack*)malloc(sizeof(Stack)); //alocar espaço para a stack
-    s->sp = -1; //inicializa stack pointer em -1, ou seja, stack está vazia
-    s->capacity = 10240;
-    s->elements = (long*)malloc(s->capacity * sizeof(long));
+    Stack* s = (Stack*)malloc(sizeof(Stack)); ///< Aloca espaço para a stack
+    s->sp = -1; ///< inicializa stack pointer em -1, ou seja, stack está vazia
+    s->capacity = 10240; ///< Capacidade inicial da stack
+    s->elements = (long*)malloc(s->capacity * sizeof(long)); ///< Aloca espaço para o array que contém os elementos da stack de acordo com a capacity
     
     return s;
 } 
 
-//Verifica se a stack está vazia
+/**
+ * \brief Função que verifica se a stack está vazia
+ * 
+ * @param s Stack
+ * 
+ * @returns s->sp == -1 , Se a stack estiver vazia retorna 1, caso contrário retorna 0
+ */
 int empty_stack(Stack* s) {
     
-    return (s->sp == -1); //se for verdadeiro 1, se for falso 0 
+    return (s->sp == -1); 
 }
 
-//Verifica se a stack está cheia
+/**
+ * \brief Função que verifica se a stack está cheia
+ * 
+ * @param s Stack
+ * 
+ * @returns s->sp == s->capacity - 1 , capacity - 1 é o último elemento do array, caso este esteja preenchido, a stack está cheia
+ */
 int full_stack(Stack* s) {
 
-    return (s->sp == s->capacity - 1); //capacity - 1 é o último elemento do array, caso esteja preenchido, logo, a stack está cheia
+    return (s->sp == s->capacity - 1); 
 
 }
 
-//Adicionar elementos na stack
-
-void push(Stack* s, long n) { //n é o elemento que será adicionado
+/**
+ * \brief Função que adiciona um elemento no topo da stack
+ * 
+ * @param s e n , Stack e elemento que será adicionado à stack, respetivamente
+ */
+void push(Stack* s, long n) {
      
     if (full_stack(s)) {
         
-        s->capacity *= 2; //duplica a capacidade da stack
-        s->elements = realloc(s->elements,s->capacity * sizeof(long)); //realloc desta nova capacidade
+        s->capacity *= 2; ///< Duplica a capacidade da stack
+        s->elements = realloc(s->elements,s->capacity * sizeof(long)); ///< Realoca o espaço na memória do array que contém os elementos da stack para esta nova capacity
         
     }
-    s->elements[++s->sp] = n;
+    s->elements[++s->sp] = n; ///< Adiciona o elemento à stack e atualiza o stack pointer para a posição deste elemento
 
     
 }
 
-//Remover elemento do topo stack
 
+/**
+ * \brief Funçao que remove o elemento do topo da stack
+ * 
+ * @param s Stack
+ * 
+ * @returns s->elements[s->sp--] , Stack sem o elemento do topo e com o stack pointer atualizado para o elemento que estava na célula de memória anterior ao que foi removido
+ */
 int pop(Stack* s) {
-    //
     
     if (empty_stack(s)) return 0; 
 
     else {
 
-        s->capacity -= sizeof(long);
-        s->elements = realloc(s->elements, s->capacity * sizeof(long));
+        s->capacity -= sizeof(long); ///< Retira o tamanho de um long da capacity
+        s->elements = realloc(s->elements, s->capacity * sizeof(long)); ///< Realoca o espaço na memória do array que contém os elementos da stack para esta nova capacity
         
 
 
@@ -72,7 +105,11 @@ int pop(Stack* s) {
 
 }
 
-//Imprimir a stack 
+/**
+ * \brief Função que imprime a stack
+ * 
+ * @param s Stack
+ */
 void print_stack(Stack *s) {
     
     for (int i = 0; i <= s->sp; i++) {
