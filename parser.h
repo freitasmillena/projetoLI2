c06f32fd548063c999ccc796d691af663e8e4927
Inês Castro <73346645+graphics-ins@users.noreply.github.com>
2021-03-25 19:56:46 +0000

    Add files via upload

--- parser.h
+++ parser.h
@@ -1,10 +1,31 @@
+/**
+* Module : Guião1 (1a semana)
+* Description : Parser -> (Guião1) - Trabalho de LI2 2020/2021
+*
+* No primeiro guião, temos como objetivo criar um programa que fosse capaz de ler
+* um input (ex.: " 17 11 - ( 18 9 / ") e devolver um output (ex.: 52).
+*
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 #include "stack.h"
 
-
+/**
+* A função parse começa por declarar os delimitadores e chama função @create_stack do ficheiro stack.h.
+* Testamos se é um número inteiro ou real e depois fazemos o push ( função no ficheiro stack.h ) caso seja inteiro.
+* Usamos o switch (também puderia ser feito com ifs mas achamos mais 'elegante' o código apresentado desta maneira), 
+* para que dependendo do token que é lido, será feita a função que lhe está associada,
+*
+*  |  + - * /         Somar, subtrair, multiplicar e dividir
+*  |  ( )             Decrementar e incrementar um valor
+*  |  %               Módulo
+*  |  #               Exponenciação
+*  |  & | ^ ~         E, ou, xor e not (bitwise) para inteiros 
+*
+* imprimindo no final o resultado.
+*/
 
 void parse(char *line) {
     char *delims = " \t\n";
@@ -108,8 +129,7 @@ void parse(char *line) {
             }
         
     }
-    
-    print_stack(s);
+    for (int i = 0; i <= s->sp; i++) printf("%ld", s->elements[i]);
 
     putchar('\n');
 }
